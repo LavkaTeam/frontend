79284c18b4183717ed43617d4d52939d3e3afdb3
Yehor Ivashkin <ivashin.egor98@gmail.com>
2025-06-25 15:11:01 +0300

    feat: 🎸 added icon show password

--- src/components/Form/InputField/InputField.tsx
+++ src/components/Form/InputField/InputField.tsx
@@ -1,6 +1,12 @@
+import { useState } from 'react';
+
 import { Input } from '@ui/Input';
 import { Label } from '@ui/Label';
 import { ErrorIcon } from '@/components/ui/icons/ErrorIcon';
+import {
+  ClosePassword,
+  ShowPassword,
+} from '@/components/ui/icons/PasswordIcons';
 
 import styles from './InputField.module.css';
 
@@ -18,6 +24,11 @@ const InputField = ({
   onChange,
   ...rest
 }: InputFieldProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const isPassword = id === 'password';
+  const actualType = isPassword && showPassword ? 'text' : rest.type;
+
   const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
     if (id === 'telephoneNumber' && !e.target.value) {
       e.target.value = '+380';
@@ -36,11 +47,21 @@ const InputField = ({
           onFocus={handleFocus}
           onChange={onChange}
           error={!!error}
+          type={actualType}
         />
-        {error && (
-          <span className={styles.icon}>
-            <ErrorIcon />
+        {isPassword ? (
+          <span
+            className={styles.icon}
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <ShowPassword /> : <ClosePassword />}
           </span>
+        ) : (
+          error && (
+            <span className={styles.icon}>
+              <ErrorIcon />
+            </span>
+          )
         )}
       </div>
       {error && <p className={styles.error}>{error}</p>}
