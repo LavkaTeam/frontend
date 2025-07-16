6bc02017367299d08c5538850d925a788334b35f
Nazarii Fito <nazariifito@MacBook-Pro-Nazarii.local>
2025-07-17 00:40:19 +0300

    fix: 🐛 fixed errors with 'key' prop and some camelCase props

--- src/components/Filters/FilterBlock/FilterBlock.tsx
+++ src/components/Filters/FilterBlock/FilterBlock.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
 import { SelectFilter } from '../SelectFilter';
 import { PriceDropdownMenu } from '../PriceDropdownMenu';
-import { CountryDropdownMenu } from '../CountryDropdownMenu'; 
+import { CountryDropdownMenu } from '../CountryDropdownMenu';
 import { countries } from '@/data/filtersData';
-import { priceFromOptions, priceToOptions, capacityOptions, strengthOptions } from '@/data/filtersData';
+import {
+  priceFromOptions,
+  priceToOptions,
+  capacityOptions,
+  strengthOptions,
+} from '@/data/filtersData';
 import styles from './FilterBlock.module.css';
 
 const FilterBlock = () => {
@@ -16,41 +21,37 @@ const FilterBlock = () => {
         <span className={styles.label}>$0-100</span>
         <div className={styles.PriceBox}>
           <PriceDropdownMenu
-            label="From"
+            label='From'
             options={priceFromOptions}
             selectedValue={fromValue}
             onSelect={setFromValue}
           />
           <PriceDropdownMenu
-            label="To"
+            label='To'
             options={priceToOptions}
             selectedValue={toValue}
             onSelect={setToValue}
-            />
+          />
         </div>
       </div>
       <span className={styles.divider}></span>
       <div className={styles.FilterBox}>
-        <CountryDropdownMenu
-          countries={countries}
-        />   
+        <CountryDropdownMenu countries={countries} />
       </div>
       <div className={styles.FilterBox}>
         <span className={styles.label}>Capacity:</span>
         <div className={styles.SelectFilterBox}>
           {capacityOptions.map((option) => (
-            <SelectFilter
-              key={option}
-              title={option}
-            />
+            <SelectFilter key={option} title={option} />
           ))}
         </div>
       </div>
       <div className={styles.FilterBox}>
         <span className={styles.label}>Strength:</span>
         <div className={styles.SelectFilterBox}>
-          {strengthOptions.map((option) => (
+          {strengthOptions.map((option, index) => (
             <SelectFilter
+              key={`strength-${index}`}
               title={option.Title}
               subtitle={option.subTitle}
             />
@@ -58,15 +59,16 @@ const FilterBlock = () => {
         </div>
       </div>
       <div className={styles.FilterBox}>
-          <div className={styles.ToggleBox}>
-              <input
-                type='checkbox' id='check'
-                className={styles.ToggleButtonInput}
-                defaultChecked
-              />
-              <label htmlFor='check' className={styles.ToggleButton}></label>   
-              <span className={styles.ToggleLabel}>Show promotions</span>
-          </div>
+        <div className={styles.ToggleBox}>
+          <input
+            type='checkbox'
+            id='check'
+            className={styles.ToggleButtonInput}
+            defaultChecked
+          />
+          <label htmlFor='check' className={styles.ToggleButton}></label>
+          <span className={styles.ToggleLabel}>Show promotions</span>
+        </div>
       </div>
       <span className={styles.divider}></span>
       <button className={styles.ClearButton}>Clear filters</button>
