1f498fd31e96ef488acd8bfda2b825c18009890c
Yehor Ivashkin <ivashin.egor98@gmail.com>
2025-10-09 17:12:26 +0300

    fix: 🐛 fix

--- src/components/HeaderIcon/HeaderIcon.tsx
+++ src/components/HeaderIcon/HeaderIcon.tsx
@@ -1,5 +1,4 @@
 import type { ReactNode } from 'react';
-
 import styles from './HeaderIcon.module.css';
 
 interface HeaderIconProps {
@@ -10,17 +9,10 @@ interface HeaderIconProps {
 const HeaderIcon = ({ icon, iconText }: HeaderIconProps) => {
   return (
     <div className={styles.headerIcon}>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <span>{icon}</span>
-=======
-      {icon}
->>>>>>> origin/master
-=======
-      <span>{icon}</span>
->>>>>>> origin/feature/create-product-page
       <span>{iconText}</span>
     </div>
   );
 };
+
 export { HeaderIcon };
